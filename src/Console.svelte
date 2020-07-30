6a53d38bd857cdaeabc08dd2cde190608fc41f4d
Andrew Roberts <andrew.jacob.roberts@gmail.com>
2020-07-30 14:49:47 -0400

    Add airline logos & update layout of distance panel

--- src/Console.svelte
+++ src/Console.svelte
@@ -11,7 +11,13 @@
 <div class="p-4 overflow-hidden bg-white rounded-lg shadow sm:p-6">
   <Tabs bind:focusedOption bind:isExpanded />
   {#if isExpanded}
-    <div transition:slide class="w-full m-2 h-72">
+    <div transition:slide class="w-full mt-4 h-72">
+      <!-- header -->
+      <div class="flex items-center p-3 rounded-t-lg bg-gray-50">
+        <div class="tracking-wider text-gray-500 uppercase w-36">Name</div>
+        <div class="ml-4 tracking-wider text-gray-500 uppercase w-28">Identifier</div>
+        <div class="ml-4 tracking-wider text-gray-500 uppercase">Distance</div>
+      </div>
       <DistanceList />
     </div>
   {/if}
--- src/DistanceList.svelte
+++ src/DistanceList.svelte
@@ -2,8 +2,20 @@
   import { distanceStatsCache } from "./stores";
   import VirtualList from "@sveltejs/svelte-virtual-list";
   import DistanceListItem from "./DistanceListItem.svelte";
+
+  let dummyArray = [
+    { id: "SWA", distance: 82739.707875192762 },
+    { id: "AAL", distance: 75246.193329843256 },
+    { id: "DAL", distance: 45041.926220355119 },
+    { id: "UAL", distance: 29202.606273851223 },
+    { id: "JBU", distance: 18581.967440776476 },
+  ];
 </script>
 
-<VirtualList items={$distanceStatsCache} let:item>
+<VirtualList
+  items={$distanceStatsCache.sort((a, b) => {
+    a.distance - b.distance;
+  })}
+  let:item>
   <DistanceListItem {...item} />
 </VirtualList>
